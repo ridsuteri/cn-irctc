b1c23d139aab95ab1fc68f1d2815daae0238f319
Riddhi Suteri <ridsuteri@gmail.com>
2025-01-12 15:46:07 +0530

    add initial code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Homepage from "./pages/Homepage/Homepage";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+
+const App = () => {
+  return (
+    <div>
+      <Register/>
+    </div>
+  );
+};
+
+export default App;
