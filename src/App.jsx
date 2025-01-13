9d34e1f8fc578a57178439c55b2c503538f19218
Riddhi Suteri <ridsuteri@gmail.com>
2025-01-13 10:15:50 +0530

    add class 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Homepage from "./pages/Homepage/Homepage";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import ClassBasedComponent from "./pages/ClassBasedComponent";
+import FunctionalComponent from "./pages/FunctionalComponent";
 
 const App = () => {
   return (
     <div>
-      <Register/>
+      <Homepage/>
+      {/* <Login/> */}
+      {/* <ClassBasedComponent initialCount = {10} />
+      <hr />
+      <FunctionalComponent  initialCount = {10} /> */}
     </div>
   );
 };
